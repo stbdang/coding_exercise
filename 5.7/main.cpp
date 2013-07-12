f4104fa3ca72214b403451657f63a6f89b3cbc66
Daniel Kim <daniel.sanghkim@gmail.com>
2013-07-11 20:55:01 -0700

    Added solution for 5.7

--- 5.7/main.cpp
+++ 5.7/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <string.h>
 
-int findMissingInteger(int A[])
+int getBit(int A[], int i, int j)
+{
+  std::cout << i << "th elem is " << A[i] << std::endl;
+  return (A[i] & (1 << j)) ? 1 : 0;
+}
+
+int findMissingInteger(int A[], int N)
 {
   // Only accessing individual bit of A[i] is allowed : try to find an O(n) algorithm
   // Adding all values to find the missing on is O(n log(n)) - not good enough!
@@ -54,46 +61,86 @@ int findMissingInteger(int A[])
   // lets use example 0-7 
   // 000 001 010 011 100 101 110 111 -> say 011 is missing (3)
 
-  // we know ans must be (if all exists) 000
+  // We know how many 1s at bit 0 needs to be present (N / 2 = 4)
 
+  // count number of 1s, we wiil find 3 -> bit 0 is 1
 
-  // bit 0 -> 1 (we now know the missing number has bit 0 set "xx1")
-  // we can discard all numbers that ends with 0 because it doesn't matter.
+  // We know we have all of number which ends with 0, we can caculate how many ones in bit 1 for those number (N / 4 = 2)
 
-  // going thru numbers that ends with 1, we find how many ones in bit 1 -> 1
-  // again, we know
+  // We also know how many ...... Isn't this too complicated?
 
+  // Try the first loop
   
+  int missingNum = 0;
 
-  int *notInterested = new int [sizeof(A)];
+  int cursor = 0;
+ 
+  int notInterested[N];
 
-  int N = sizeof(A) + 1;
-  int ans = 0;
-  int shift = 0;
+  memset(notInterested, 0, sizeof(notInterested));
 
-  while (N){
-    N >> 1;
-    ans |= (N % 2) dd<< shift;
-    shift++;
-  }
+  std::cout << "Upper boundary : " << N << std::endl;
 
-  N = sizeof(A);
+  while (N >> cursor) {
+  
+    std::cout << "Looking at bit " << cursor << std::endl;
+    int numOnes;
 
-  for (int j = 0; j < shift; j++) {
+    if (cursor == 0)
+      numOnes = (N) / 2;
+    else {// general case
+      int chunk = (2 << cursor);
+      numOnes = (N) / chunk;
+      int rem = (N) % chunk;
+      if ( rem >= (missingNum | (1 << cursor)))
+        numOnes ++; 
+    }
 
-    int result = 0;
+    std::cout << "Number of ones should be " << numOnes << std::endl;
+    int count = 0;
+
     for (int i = 0; i < N; i++)
     {
-      if ( !notInterested[i] ) {
-        result ^= getBit(A, i, j);
-
+      if (!notInterested[i]) {
+        if (getBit(A, i, cursor))
+          count++;
       }
     }
+
+    std::cout << "Ones counted " << count << std::endl;
+    missingNum |= (numOnes - count) << cursor;
+
+    for (int i = 0; i < N; i++)
+    {
+      if (!notInterested[i]) {
+        if (getBit(A,i,cursor) != (numOnes - count))
+          notInterested[i] = 1;
+      }
+    }
+    cursor++;
   }
+
+  return missingNum;
+
 }
 
 int main()
 {
+  int N = 100;
+  int omit = 34;
+
+  int A[N];
+  int ind = 0;
+
+  for (int i=0; i<N+1; i ++ ) {
+    if (i != omit) {
+      A[ind] = i;
+      ind++;
+    }  
+  }
+
+
+  std::cout << "Missing num is " << findMissingInteger(A, N) << std::endl;
 
   return 0;
 }
